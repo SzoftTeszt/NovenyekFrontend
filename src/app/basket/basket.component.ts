c69bc2248fc8764b83beba72b56cb5a1ff1317e3
Erős István <eros@zold.lan>
2023-11-08 20:05:25 +0100

    20:05

--- src/app/basket/basket.component.ts
+++ src/app/basket/basket.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { KosarService } from '../kosar.service';
+import { BaseService } from '../base.service';
 
 @Component({
   selector: 'app-basket',
@@ -6,5 +8,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./basket.component.css']
 })
 export class BasketComponent {
+  tetelek:any
+  novenyek:any=[]
 
+  constructor(
+    private kosar:KosarService,
+    private base:BaseService
+  ){
+    this.kosar.getKosarban().subscribe(
+      (a)=> this.tetelek=a
+    )
+
+    this.base.getPlants().subscribe(
+      (adatok:any)=> this.novenyek=adatok
+    )
+
+  }
+
+  keresNoveny(id:any){
+    let n= this.novenyek.filter(
+      (n:any)=>n.id==id
+    )
+
+    console.log("Növény",n)
+    return n[0];
+  }
+
+  tetelOsszes(tetel:any){
+    let noveny = this.keresNoveny(tetel.id)
+    // console.log("db",tetel.db)
+    // console.log("ar",noveny.ar)
+    let osszes= Number(tetel.db)*Number(noveny.ar)
+    // console.log(osszes,"Ft")
+    return osszes
+  }
+
+  osszesen()
+  {
+    let fizetendo=0
+    this.tetelek.forEach(
+      (e:any)=> fizetendo+= this.tetelOsszes(e)
+    )
+    return fizetendo
+  }
+
+  torol(tetel:any){
+    this.kosar.torol(tetel)
+  }
 }
--- src/app/kosar.service.ts
+++ src/app/kosar.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class KosarService {
   kosar:any=[]
+  kosarban=new Subject()
+
+  getKosarban(){
+    return this.kosarban
+  }
+
   constructor() { }
 
   addTetel(id:any, db:any){
@@ -14,7 +21,16 @@ export class KosarService {
     )
     if (i>=0) this.kosar[i].db=db
     else this.kosar.push(body)
-
+    
+    this.kosarban.next(this.kosar)
     console.log("Kosár:", this.kosar)
   }
+
+torol(tetel:any){
+  this.kosar=this.kosar.filter(
+    (e:any)=>e!=tetel
+  )
+  this.kosarban.next(this.kosar)
+}
+
 }
